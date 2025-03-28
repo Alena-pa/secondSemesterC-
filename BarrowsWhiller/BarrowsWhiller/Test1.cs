7c9b378ac344c385e6d2cd96373ff5be81961755
Alena-pa <alena.pakhnusheva@gmail.com>
2025-03-28 16:58:59 +0300

    finish tests and write burowsWheller and rename

--- BarrowsWhiller/BarrowsWhiller/Test1.cs
+++ BarrowsWhiller/BarrowsWhiller/Test1.cs
@@ -1,11 +1,39 @@
-﻿namespace BarrowsWhiller
+﻿using BurrowsWheeler;
+
+namespace BarrowsWhillerTests
 {
     [TestClass]
     public sealed class Test1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestBWTValidInput()
         {
+            var input = "banana$";
+
+            var result = BurrowsWheelerTransform.BWT(input);
+
+            Assert.AreEqual("annb$aa", result);
+        }
+
+        [TestMethod]
+        public void TestReverseBWTValidInput()
+        {
+            var bwtInput = "annb$aa";
+            var startIndex = 4;
+
+            var result = BurrowsWheelerTransform.ReverseBWT(bwtInput, startIndex);
+
+            Assert.AreEqual("banana$", result);
+        }
+
+        [TestMethod]
+        public void TestBWTEmptyInput()
+        {
+            var input = "";
+
+            var result = BurrowsWheelerTransform.BWT(input);
+
+            Assert.IsNull(result);
         }
     }
 }
