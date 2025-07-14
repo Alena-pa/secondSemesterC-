8067340c4196ca34c6754e1acb921e23af7609c1
Alena-pa <alena.pakhnusheva@gmail.com>
2025-07-14 21:27:31 +0300

    start doing LZW, add Compress, start Decompress

---
+++ LZW/LZW/Lzw.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrieDatatype;
+
+namespace LZW;
+
+public class LZWAlgorythm
+{
+
+    public static (string, Trie) Compress(string text)
+    {
+        Trie trie = new Trie();
+
+        foreach (var i in text)
+        {
+            trie.Add(i.ToString());
+        }
+
+        var phrase = "";
+        var output = "";
+
+        foreach (char symbol in text)
+        {   
+            if (trie.Contains(phrase + symbol.ToString()))
+            {
+                phrase = phrase + symbol.ToString();
+            }
+            else
+            {
+                trie.Add(phrase + symbol.ToString());
+                output += trie.PhraseCode(phrase);
+                phrase = symbol.ToString();
+            }
+        }
+        output += trie.PhraseCode(phrase);
+        return (output, trie);
+    }
+
+    public static string Decompress(string text, Trie trie)
+    {
+        Trie trieWithCodes = new Trie();
+
+        foreach (var i in text)
+        {
+            trieWithCodes.Add(i.ToString());
+        }
+
+        var phrase = "";
+        var output = "";
+
+        foreach (char code in text)
+        {
+            
+        }
+        output += trie.PhraseCode(phrase);
+        return output;
+    }
+}
---
+++ LZW/LZW/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using LZW;
+using TrieDatatype;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter text you want to compress");
+        string text = Console.ReadLine();
+        if (text == null) 
+        {
+            throw new Exception("Cannot find text");
+        }
+        var output = LZWAlgorythm.Compress(text);
+        Console.WriteLine(output);
+    }
+}
